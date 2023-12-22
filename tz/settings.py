e733a9f2c6d3007ecac63846fa57c9bfe2398172
ugaly <mwalwamaj@gmail.com>
2023-12-22 17:39:14 +0300

    updtd

--- tz/settings.py
+++ tz/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 4,
 }
 
 
